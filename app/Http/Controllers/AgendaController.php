5a5c318c4a0a213ae3bc1c117c02fd0fa80586f2
Edrian <edrianaalar@su.edu.ph>
2023-08-01 19:25:08 +0800

    Adding agendas and fixing some errors

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Agenda;
 
 class AgendaController extends Controller
 {
-    //
+    public function getAgenda()
+    {
+        return response()->json(Agenda::latest()->get());
+    }
+    
+    public function storeAgenda(Request $request)
+    {
+        Agenda::create([
+            'title' => $request->title,
+            'message' => $request->message,
+            'type' => $request->type,
+            'author' => $request->author,
+        ]);
+
+        return response()->json('Agenda Successfully Created');
+    }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,29 +1,149 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-</script>]
+import axios from 'axios';
+</script>
 
 <template>
     <Head>
-        <title>Home Page</title>
-         <link href="/css/app.css" rel="stylesheet">
-         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Home Page</title>    
     </Head>
 
     <AuthenticatedLayout>
-        <div class="py-14">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="p-6 text-gray-900">Welcome To Tasks</div>
+       <div class="row justify-content-center mx-auto">
+            <div class="col-md-8 py-4">
+                <div class="card">
+                    <div class="card-header bg-white overflow-hidden shadow-m sm:rounded-lg">
+                        <div class="row">
+                            <div class="col-md-6">
+                                  <h5 class="text-gray-900 float-start">Welcome To Agendas</h5>
+                            </div>
+                            <div class="col-md-6">
+                                <button @click="createAgenda" class="btn btn-success btn-m float-right">Add Agenda</button>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table text-center">
+                                <thead>
+                                <tr>
+                                    <th>Agenda #</th>
+                                    <th>Title</th>
+                                    <th>Message</th>
+                                    <th>Type</th>
+                                    <th>Author</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(item,index) in agenda" :key="index">
+                                        <td>{{ index + 1 }}</td>
+                                        <td>{{ item.title }}</td>
+                                        <td>{{ item.message.length <=20 ? item.message : item.message.subtr(0,20) + '...' }}</td>
+                                        <td>{{ item.type }}</td>
+                                        <td>{{ item.author }}</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                         <div class="col-md-6">
-                            <button class="btn btn-sucess btn-sm float-end">Add Agenda</button>
+                    </div>
+                </div> <!--end of card-->
+            </div> 
+       </div>
+    
+       <!--Modal for adding-->
+        <div class="modal fade" id="agendaModal" tabindex="-1" aria-labelledby="agendaModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title fs-5" id="agendaModalLabel">Adding Agenda</h5>
+                        <button  class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <form class="form-group" action="./api/storeAgenda" method="post" @submit.prevent="storeAgendaAgenda()">
+                                <div class="col-md-10">
+                                    <div class="form-group">
+                                        <label for="title">Title:</label>
+                                        <input type="text" class="form-control"  v-model="taskData.title" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-10">
+                                        <div class="form-group">
+                                            <label for="message">Message:</label>
+                                            <input type="text" class="form-control" v-model="taskData.message" required>
+                                        </div>
+                                </div>
+                                <div class="col-md-10">
+                                        <div class="form-group">
+                                            <label for="type">Type:</label>
+                                            <input type="text" class="form-control" v-model="taskData.type" required>
+                                        </div>
+                                </div>
+                                <div class="col-md-10">
+                                        <div class="form-group">
+                                            <label for="author">Author:</label>
+                                            <input type="text" class="form-control" v-model="taskData.author" required>
+                                        </div>
+                                </div>
+                              </form>  
                         </div>
+                    </div> <!--end of modal body-->
+                    <div class="modal-footer">
+                        <button  class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button @click="storeAgenda" class="btn btn-primary">Add</button>
                     </div>
                 </div>
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            taskData:{
+                title:'',
+                message: '',
+                type: '',
+                author: '',
+            },
+            agenda: {},
+        }
+    },
+    mounted(){
+        this.getAgenda()
+    },
+    methods: {
+        getAgenda(){
+            axios.get('./api/getAgenda').then(response => {
+                this.agenda = response.data
+            }).catch(error => {
+                console.log(error)
+            })
+        },
+        createAgenda(){
+            this.taskData = {
+                title: '',
+                message: '',
+                type: '',
+                author: '', 
+            }
+            $('#agendaModal').modal('show')
+        },
+        storeAgenda(){
+            axios.post('./api/storeAgenda', this.taskData).then(response => {
+                console.log(response.data)
+            }).catch(error => {
+                console.log(error)
+            }).finally(() => {
+                $('#agendaModal').modal('hide')
+            }
+            )
+        }
+    }
+};
+</script>
