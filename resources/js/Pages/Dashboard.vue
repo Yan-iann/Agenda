04425a9f924b46b52f966324ccf4a27b548a7317
Edrian <edrianaalar@su.edu.ph>
2023-08-01 16:05:20 +0800

    Bootstrap modified

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,16 +1,27 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-</script>
+</script>]
 
 <template>
-    <Head title="Home Page" />
+    <Head>
+        <title>Home Page</title>
+         <link href="/css/app.css" rel="stylesheet">
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </Head>
 
     <AuthenticatedLayout>
-        <div class="py-12">
+        <div class="py-14">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="p-6 text-gray-900">Welcome To Tasks</div>
+                        </div>
+                         <div class="col-md-6">
+                            <button class="btn btn-sucess btn-sm float-end">Add Agenda</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
